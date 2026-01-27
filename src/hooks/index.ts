4859eaf79ebd6168b68a16786f86c488dd585d98
aidaptivecom-pixel <aidaptive.com@gmail.com>
2026-01-27 12:45:45 -0300

    feat: export new hooks (useLeads, useConversations, useMessages)

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,4 @@
 export { useProjects, useProject } from './useProjects'
 export { useAgents } from './useAgents'
+export { useLeads, useLead } from './useLeads'
+export { useConversations, useMessages } from './useConversations'
