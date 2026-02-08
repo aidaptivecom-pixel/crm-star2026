249e026ff6b713837575732a79609798e70d55ac
AidaptiveCore <aidaptivecore@Mac-mini-de-AidaptiveCore.local>
2026-02-08 09:29:30 -0800

    fix: correct API field mapping + response parsing + fullscreen iframe in dashboard

--- src/pages/TasacionWeb.tsx
+++ src/pages/TasacionWeb.tsx
@@ -1,10 +1,9 @@
 export const TasacionWeb = () => {
   return (
-    <div className="h-full w-full -m-6">
+    <div className="fixed inset-0 left-[240px] max-lg:left-0 z-10 bg-white">
       <iframe
         src="/tasacion-web.html"
         className="w-full h-full border-0"
-        style={{ minHeight: 'calc(100vh - 64px)' }}
         title="Tasación Web - Star Real Estate"
       />
     </div>
